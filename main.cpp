851a7a637400214ecfc67cea8a47121d81f1ff86
alextwix <alextwix69@gmail.com>
2025-11-30 18:29:13 +0300

    + taskname

--- main.cpp
+++ main.cpp
@@ -1,30 +1,47 @@
 #include "Header.h"
 #include <clocale>
 #include <iostream>
+#include <stdexcept>
 using namespace std;
+
 int main() {
-	setlocale(LC_ALL, "ru");
+	while (true) {
 
-	ifstream file;
-	file.open("tasks.txt", ifstream::in);
-	filePrint(file); //try - catch
-	file.close();
+		setlocale(LC_ALL, "ru");
 
-	cout << endl;
-	char dec = {}; //decision
-	int i = 0;
-	while (dec - '0' > 3 || dec - '0' < 1) {
-		if (i > 0) {
-			cout << "введите цифру от 1 до 3" << endl;
+		ifstream file;
+		file.open("tasks.txt", ifstream::in);
+		try {
+			filePrint(file);
+		}
+		catch(const runtime_error& e) {
+			cout << e.what() << endl;
+			break;
+		}
+			
+		file.close();
+
+		cout << endl;
+		char dec = {}; //decision
+		int i = 0;
+		while (dec - '0' > 4 || dec - '0' < 1) {
+			if (i > 0) {
+				cout << "введите цифру от 1 до 3" << endl;
+			}
+
+			cout << "1 - добавить задачу" << endl
+				<< "2 - изменить статус задачи" << endl
+				<< "3 - удалить задачу из списка" << endl
+				<< "4 - сохранить и выйти" << endl;
+			cin >> dec;
+
+			i++;
 		}
 
-		cout << "1 - добавить задачу" << endl
-			<< "2 - изменить статус задачи" << endl
-			<< "3 - удалить задачу из списка" << endl;
-		cin >> dec;
+		if (dec == '1') {
 
-		i++;
+		}
+
 	}
-	
 	return 1;
 }
