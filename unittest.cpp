48a36be1b9deb90f1dcd5501eec5d5da33d846e1
alextwix <alextwix69@gmail.com>
2025-12-30 13:02:12 +0300

    test: + testcases for ToDolist class functions: getQuant(), addTask(), renameTask(), switchTask(); all have submitted

--- unittest.cpp
+++ unittest.cpp
@@ -1,6 +1,57 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "header.h"
 
-TEST_CASE("1") {
-    CHECK(2 + 2 == 4);
+TEST_CASE("ToDoList class test") {
+    ToDoList testList;
+    Task* testTask = new Task;
+
+    SUBCASE ("getQuant() | addTask()") {
+        CHECK(testList.getQuant() == 0);
+
+        testList.addTask(testTask);
+
+        CHECK(testList.getQuant() == 1);
+
+        for (int i = 0; i < 9; i++) {
+            testList.addTask(testTask);
+        }
+
+        CHECK(testList.getQuant() == 10);
+    }
+
+    SUBCASE ("renameTask()") {
+        CHECK(testTask->name == "new task");
+
+        testList.renameTask(*testTask, "test name");
+
+        CHECK(testTask->name == "test name");
+
+        try {
+            testList.renameTask(*testTask, "");
+        }
+        catch (const invalid_argument& e) {
+            CHECK(e.what() == string("invalid name"));
+        }
+
+        try {
+            testList.renameTask(*testTask, "this name is too long to be valid 676767676767676767676767676767");                                                                      
+        }
+        catch (const invalid_argument& e) {
+            CHECK(e.what() == string("invalid name"));
+        }
+        
+    }
+
+    SUBCASE ("switchTask()") {
+        CHECK(testTask->completed == false);
+
+        testList.switchTask(*testTask);
+
+        CHECK(testTask->completed == true);
+
+        testList.switchTask(*testTask);
+
+        CHECK(testTask->completed == false);
+    }
 }
